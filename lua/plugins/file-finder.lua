cf9b366d8899c06f7519e5ed913bd444399c7014
Kovalski-rgb <andre.kovalski@tecpuc.com.br>
2024-03-01 17:17:24 -0300

    checkpoint telescope with ignore files

--- lua/plugins/file-finder.lua
+++ lua/plugins/file-finder.lua
@@ -17,13 +17,23 @@ return {
 	},
 
 	config = function ()
-
+		local telescope = require('telescope')
 		local builtin = require('telescope.builtin')
 
 		vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 		vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 		vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 		vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+		telescope.setup({
+			defaults = {
+				file_ignore_patterns = {
+					-- for angular/node projects
+					"node%_modules/.*", "dist/.*", "coverage/.*", ".vscode/.*", "e2e/.*"
+				}
+			}
+		})
+
 
 	end
 
