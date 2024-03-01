6e5fcb1169d5f61ad3b4ca173de10d7f31149eb8
Kovalski-rgb <andre.kovalski@tecpuc.com.br>
2024-03-01 12:19:08 -0300

    checkpoint mason-lsp

--- lua/plugins/language-suport.lua
+++ lua/plugins/language-suport.lua
@@ -3,15 +3,18 @@ return {
 
 	dependencies = {
 		"williamboman/mason.nvim",
+		"williamboman/mason-lspconfig.nvim",
 	},
 
 	config = function()
 		-- local instances
 		local lspconfig	= require("lspconfig")
 	 	local mason = require("mason")
+	 	local mason_lsp = require("mason-lspconfig")
 
 		-- setups
 		mason.setup()
+		mason_lsp.setup()
 		lspconfig.pyright.setup({})
 
 	end
